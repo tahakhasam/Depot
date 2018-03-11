59373e310f11dcf25c931e40479545c1d5eab758
Taha Khasamwala <tahakhasam@gmail.com>
2018-03-11 22:14:26 +0530

    added Add to Cart button and its functionality and also test for the same.

--- app/models/product.rb
+++ app/models/product.rb
@@ -27,7 +27,7 @@ class Product < ApplicationRecord
 		#ensure that there are no line_items referencing this product
 		def ensure_not_referenced_by_any_line_item
 			unless line_items.empty?
-				errors.add(:base, 'Line Items preseent')
+				errors.add(:base, 'Line Items present')
 				throw :abort
 			end
 		end
