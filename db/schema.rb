1136e279da0e91b786e346d85aabc71d7cacf6fc
Taha Khasamwala <tahakhasam@gmail.com>
2018-03-12 15:53:27 +0530

    changed cart display from ul to table added invalid cart and overall made the cart much smarter.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312030522) do
+ActiveRecord::Schema.define(version: 20180312035653) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
